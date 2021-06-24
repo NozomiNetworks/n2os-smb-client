5f89a3de7ea09de03c0d27326e3a066ed6e29e49
Davide Di Gennaro <70326930+davide-digennaro-nozomi@users.noreply.github.com>
2021-06-24 10:07:58 +0200

    make Kerberos optional (#3)
    * make krb5 optional
    
    * add scripts
    
    * update binary
    
    * linux update

---
+++ build.linux.sh
@@ -0,0 +1,12 @@
+#/bin/sh
+
+apt update
+apt install -y git build-essential upx cmake libssl-dev libkrb5-dev
+
+git submodule update --init --recursive
+cmake . -DCMAKE_BUILD_TYPE=Release
+make
+strip -s n2os_smb_client
+upx --best n2os_smb_client
+
+cp n2os_smb_client bin/n2os_smb_client.linux
